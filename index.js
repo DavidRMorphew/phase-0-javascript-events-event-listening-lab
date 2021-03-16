c92b02b166416603f5fd5ca770e79215c6291260
DavidRMorphew <morphewd@umich.edu>
2021-03-16 19:24:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const input = document.getElementById('input')
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) {
+        console.log(event);
+    });
+    
 }
+
+
+// const input = document.querySelector('input');
+
+// function addingEventListener() {
+//   input.addEventListener('click', function(event) {
+//     return "clicked"
+//   });
+// }
